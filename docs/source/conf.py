046713b70155ae778c13056ef8e327f49f51fb82
Téo Bouvard <teobouvard@gmail.com>
2019-10-11 17:43:11 +0200

    Mock import C API

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+autodoc_mock_imports = ['geolib']
 
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
