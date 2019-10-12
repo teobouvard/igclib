1325888edd16e298a5aff36b4d4c1eb0cc65cc50
Téo Bouvard <teobouvard@gmail.com>
2019-10-13 01:25:19 +0200

    Removed C useless type

--- igclib/c_ext/geolib.c
+++ igclib/c_ext/geolib.c
@@ -14,13 +14,9 @@
 
 /* TYPES */
 
-typedef struct t_pos{
+typedef struct t_wp{
     double lat;
     double lon;
-} t_pos;
-
-typedef struct t_wp{
-    t_pos center;
     double radius;
 } t_wp;
 
@@ -43,11 +39,11 @@ double c_haversine(double lat1, double lon1, double lat2, double lon2){
 	return asin(sqrt(dx * dx + dy * dy + dz * dz) / 2) * 2 * R;
 }
 
-t_pos c_offset(double lat1, double lon1, double distance, double heading){
+t_wp c_offset(double lat1, double lon1, double distance, double heading){
     lat1 *= TO_RAD, lon1 *= TO_RAD, heading *= TO_RAD;
     double lat2 =  asin(sin(lat1) * cos(distance/R)  + cos(lat1) * sin(distance/R) * cos(heading));
     double lon2 = lon1 + atan2(sin(heading) * sin(distance/R) * cos(lat1) , cos(distance/R) - sin(lat1) * sin(lat2));
-    t_pos end_point = {lat2 *= TO_DEG, lon2 *= TO_DEG};
+    t_wp end_point = {lat2 *= TO_DEG, lon2 *= TO_DEG, 0};
     return end_point;
 }
 
@@ -59,10 +55,9 @@ double c_heading(double lat1, double lon1, double lat2, double lon2){
     return heading;
 }
 
-void c_optimize(t_pos position, vc_vector *wpts, int nb_wpts, t_result *res){
+void c_optimize(t_wp position, vc_vector *wpts, int nb_wpts, t_result *res){
     double in_heading, out_heading, pivot_heading;
-    double in_distance, out_distance, pivot_distance;
-    double next_distance;
+    double in_distance, out_distance, pivot_distance, next_distance;
     double angle;
 
     t_wp fwp;
@@ -74,7 +69,7 @@ void c_optimize(t_pos position, vc_vector *wpts, int nb_wpts, t_result *res){
     if (nb_wpts < 2){
         last_wp  = vc_vector_back(wpts);
         vc_vector_push_back(res->fast_wp, last_wp);
-        res->dist_opti = c_haversine(position.lat, position.lon, last_wp->center.lat, last_wp->center.lon);
+        res->dist_opti = c_haversine(position.lat, position.lon, last_wp->lat, last_wp->lon);
         res->legs_dist[0] = res->dist_opti;
     } 
 
@@ -85,9 +80,9 @@ void c_optimize(t_pos position, vc_vector *wpts, int nb_wpts, t_result *res){
             t_wp *two = vc_vector_at(wpts, i);
             t_wp *three = vc_vector_at(wpts, i+1);
 
-            in_heading = c_heading(two->center.lat, two->center.lon, one->center.lat, one->center.lon);
-            in_distance = c_haversine(two->center.lat, two->center.lon, one->center.lat, one->center.lon);
-            out_distance = c_haversine(two->center.lat, two->center.lon, three->center.lat, three->center.lon);
+            in_heading = c_heading(two->lat, two->lon, one->lat, one->lon);
+            in_distance = c_haversine(two->lat, two->lon, one->lat, one->lon);
+            out_distance = c_haversine(two->lat, two->lon, three->lat, three->lon);
 
             if (out_distance <  MIN_WP_DIST){
                 out_heading = 0; // TODO
@@ -95,26 +90,26 @@ void c_optimize(t_pos position, vc_vector *wpts, int nb_wpts, t_result *res){
                 pivot_heading = in_heading; // TODO
             }
             else{
-                out_heading = c_heading(two->center.lat, two->center.lon, three->center.lat, three->center.lon);
+                out_heading = c_heading(two->lat, two->lon, three->lat, three->lon);
                 angle = fmod(out_heading - in_heading + 540.0, 360.0) - 180.0;
                 pivot_heading = in_heading  + 0.5 * angle;
                 pivot_distance = (2.0 * in_distance * out_distance * cos(angle * 0.5 * TO_RAD)) / (in_distance + out_distance);
             }
 
             pivot_distance = MIN(pivot_distance, two->radius);
-            fwp.center = c_offset(two->center.lat, two->center.lon, pivot_distance, pivot_heading);
+            
+            fwp = c_offset(two->lat, two->lon, pivot_distance, pivot_heading);
             fwp.radius = two->radius;
             vc_vector_push_back(res->fast_wp, &fwp);
 
-            next_distance = c_haversine(one->center.lat, one->center.lon, fwp.center.lat, fwp.center.lon);
+            next_distance = c_haversine(one->lat, one->lon, fwp.lat, fwp.lon);
             res->legs_dist[i] = next_distance;
             res->dist_opti += next_distance;
         }
 
-        
         last_fwp = vc_vector_back(res->fast_wp);
         last_wp  = vc_vector_back(wpts);
-        next_distance = c_haversine(last_fwp->center.lat, last_fwp->center.lon, last_wp->center.lat, last_wp->center.lon);
+        next_distance = c_haversine(last_fwp->lat, last_fwp->lon, last_wp->lat, last_wp->lon);
         res->legs_dist[nb_wpts] = next_distance;
         
         vc_vector_push_back(res->fast_wp, vc_vector_back(wpts));
@@ -143,9 +138,10 @@ static PyObject* optimize(PyObject* self, PyObject* args){
         return NULL;
 
     // create C types from arguments
-    t_pos position = {
+    t_wp position = {
         PyFloat_AsDouble(PyTuple_GetItem(pos, 0)), 
-        PyFloat_AsDouble(PyTuple_GetItem(pos, 1))
+        PyFloat_AsDouble(PyTuple_GetItem(pos, 1)),
+        0
     };
 
     int nb_wpts = PyList_Size(wpts);
@@ -154,10 +150,8 @@ static PyObject* optimize(PyObject* self, PyObject* args){
     for (int i = 0; i < nb_wpts; i++){
         curr_wpt = (PyObject*) PyList_GetItem(wpts, i);
         t_wp wp = {
-            {
-                PyFloat_AsDouble(PyObject_GetAttrString(curr_wpt, "lat")),
-                PyFloat_AsDouble(PyObject_GetAttrString(curr_wpt, "lon"))
-            },
+            PyFloat_AsDouble(PyObject_GetAttrString(curr_wpt, "lat")),
+            PyFloat_AsDouble(PyObject_GetAttrString(curr_wpt, "lon")),
             PyFloat_AsDouble(PyObject_GetAttrString(curr_wpt, "radius"))
         };
         vc_vector_push_back(waypoints, &wp);
@@ -177,8 +171,8 @@ static PyObject* optimize(PyObject* self, PyObject* args){
     for (int i = 0; i < nb_wpts; i++){
         PyList_Append(legs, Py_BuildValue("d", res->legs_dist[i]));
         t_wp *wp = vc_vector_at(res->fast_wp, i);
-        PyObject_SetAttrString(curr_wpt, "lat", Py_BuildValue("d", wp->center.lat));
-        PyObject_SetAttrString(curr_wpt, "lon", Py_BuildValue("d", wp->center.lon));
+        PyObject_SetAttrString(curr_wpt, "lat", Py_BuildValue("d", wp->lat));
+        PyObject_SetAttrString(curr_wpt, "lon", Py_BuildValue("d", wp->lon));
         PyObject_SetAttrString(curr_wpt, "radius", Py_BuildValue("d", wp->radius));
         PyList_Append(fwp, curr_wpt);
     }
