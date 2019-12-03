da48eeadf2c74c3c4844457446815d77726d7213
Téo Bouvard <teobouvard@gmail.com>
2019-12-03 17:56:12 +0100

    Geospatial indexing speedup

--- igclib/core/airspace.py
+++ igclib/core/airspace.py
@@ -1,19 +1,24 @@
 import logging
 
+from igclib.geography import destination
 from igclib.geography.converters import parse_altitude
 from igclib.geography.geo import Arc
 from shapely.geometry import Polygon
-from shapely.prepared import prep
 
+NORTH = 0
+SOUTH = 180
+EAST = 90
+WEST = -90
 
 class Airspace:
 
     def __init__(self, record):
         self.name = record.get('name')
+        self.airspace_class = record.get('class')
         self.floor, self.ground_floor = parse_altitude(record.get('floor'))
         self.ceiling, self.ground_ceiling = parse_altitude(record.get('ceiling'))
         self.polygon, self.arcs = self.build(record.get('elements'))
-        self.airspace_class = record.get('class')
+        self.bounds = self.compute_bounds()
 
     def __contains__(self, point):
         if self.ground_floor and point.agl < self.floor:
@@ -50,3 +55,27 @@ class Airspace:
             logging.warning(f'{self.name} does not contain enough points to build a polygon')
             return None, arcs
         return Polygon(points), arcs
+
+    def compute_bounds(self):
+        bounds = None
+        if self.polygon:
+            bounds = list(self.polygon.bounds)
+        if self.arcs:
+            north_points = []
+            south_points = []
+            east_points = []
+            west_points = []
+            for arc in self.arcs:
+                north_points.append(destination(arc, arc.radius, NORTH)[0])
+                south_points.append(destination(arc, arc.radius, SOUTH)[0])
+                east_points.append(destination(arc, arc.radius, EAST)[1])
+                west_points.append(destination(arc, arc.radius, WEST)[1])
+            if bounds:
+                bounds[0] = min(bounds[0], min(south_points))
+                bounds[1] = min(bounds[1], min(west_points))
+                bounds[2] = max(bounds[2], max(north_points))
+                bounds[3] = max(bounds[0], max(east_points))
+            else:
+                bounds = min(south_points), min(west_points), max(north_points), max(east_points)
+        return bounds
+
--- igclib/core/xc.py
+++ igclib/core/xc.py
@@ -1,6 +1,4 @@
 import logging
-import multiprocessing
-multiprocessing.set_start_method('spawn', True)
 import os
 
 from aerofiles import openair
@@ -9,8 +7,9 @@ from igclib.core.airspace import Airspace
 from igclib.core.flight import Flight
 from igclib.geography.elevation import elevation
 from igclib.geography.geo import Point
+from rtree.index import Index
 from tqdm import tqdm
-from collections import defaultdict
+from shapely.geometry import LineString
 
 
 class XC(BaseObject):
@@ -21,6 +20,7 @@ class XC(BaseObject):
     def __init__(self, tracks=None, airspace=None, progress='gui'):
         self.flight = Flight(tracks)
         self.points = [Point(p.lat, p.lon, p.altitude) for p in self.flight.to_list()]
+        self.bounds = self.get_bounding_box()
         ground_altitude = elevation(self.flight.to_list())
         if not ground_altitude:
             self.agl_validable = False
@@ -31,10 +31,11 @@ class XC(BaseObject):
 
         if airspace is not None:
             airspace = self.read_airspace(airspace)
-            self.violations = self.validate(airspace)
+            possible_violations = [inter.object for inter in airspace.intersection(self.bounds, objects=True)]
+            self.violations = self.validate(possible_violations)
 
     def read_airspace(self, airspace):
-        zones = []
+        index = Index()
         with open(airspace, 'r') as f:
             reader = openair.Reader(f)
             for record, error in reader:
@@ -46,22 +47,19 @@ class XC(BaseObject):
                         if not self.agl_validable and (zone.ground_floor or zone.ground_ceiling):
                             logging.warning(f'{zone.name} will not be checked because ground altitude of flight could not be retrieved.')
                         else:
-                            zones.append(zone)
+                            if zone.bounds:
+                                index.insert(id(zone), zone.bounds, obj=zone)
                     except KeyError:
                         logging.warning(f'line {reader.reader.lineno} of {os.path.basename(airspace)} - error in previous record')
-        return zones
+        return index
 
-    #def validate(self, zones):
-    #    violations = {}
-    #    with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
-    #        for zone, inter in tqdm(p.map(self.check_intersections, zones), total=len(zones), desc='checking airspace intersections'):
-    #            if inter:
-    #                violations[zone.name] = inter
-    #    return violations
-
-    #def check_intersections(self, zone):
-    #    return zone, list(filter(zone.__contains__, self.points))
-
+    def get_bounding_box(self):
+        # TODO this but less stupid
+        min_x = min(self.points, key=lambda p:p.x).x
+        max_x = max(self.points, key=lambda p:p.x).x
+        min_y = min(self.points, key=lambda p:p.y).y
+        max_y = max(self.points, key=lambda p:p.y).y
+        return min_x, min_y, max_x, max_y
 
     def validate(self, zones):
         violations = {}
