cf071b6ac51de8d5575d762879ef92299bb37814
Téo Bouvard <teobouvard@gmail.com>
2020-02-07 17:30:39 +0100

    Add missing requirements

--- setup.py
+++ setup.py
@@ -3,35 +3,38 @@ from distutils.core import Extension
 
 VERSION = '0.1.1'
 
+
 def requirements():
     with open('requirements.txt', 'r') as f:
         return f.read().splitlines()
+
 
 def readme():
     with open('readme.md', 'r') as f:
         return f.read()
 
+
 geolib = Extension('geolib', ['igclib/geography/geodesic.c', 'igclib/geography/geolib.c'])
 
 setup(name='igclib',
-    version=VERSION,
-    description='A library for paragliding races',
-    long_description=readme(),
-    url='https://github.com/teobouvard/igclib',
-    author='Téo Bouvard',
-    author_email='teobouvard@gmail.com',
-    license='GPL-3',
-    packages=find_packages(include=['igclib']),
-    ext_modules = [geolib],
-    install_requires=requirements(),
-    scripts=['igclib/bin/igclib'],
-    python_requires='>=3.5',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: Unix',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-    zip_safe=True)
+      version=VERSION,
+      description='A library for paragliding races',
+      long_description=readme(),
+      url='https://github.com/teobouvard/igclib',
+      author='Téo Bouvard',
+      author_email='teobouvard@gmail.com',
+      license='GPL-3',
+      packages=find_packages(include=['igclib']),
+      ext_modules=[geolib],
+      install_requires=requirements(),
+      scripts=['igclib/bin/igclib'],
+      python_requires='>=3.5',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: Unix',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+      ],
+      zip_safe=True)
